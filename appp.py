0d3d77f4992f31ec419d448269c356ee9bbf1a78
doanhuynhanhthu13092001-commits <doanhuynhanhthu13092001@gmail.com>
2026-06-18 14:20:32 +0700

    appp.py

--- appp.py
+++ appp.py
@@ -1,5 +1,5 @@
 import streamlit as st 
-st.image("logo.jpg")
+st.image("2aOboQeLhVbnpt1uF9e21DhJb3B4Zh7Mt8hYwEQC.jpg")
 # 1. Cấu hình giao diện ứng dụng
 st.set_page_config(page_title="App Tính Thu Nhập Cá Nhân", page_icon="💰", layout="centered")
 st.title("💰 Ứng Dụng Tính Thuế TNCN & Lương Thực Nhận")
